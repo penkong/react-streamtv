e1e0613167887fc38ebfe2ec4b87121795d2d1b2
mazdak <nazemi.works@gmail.com>
2019-05-18 03:05:47 -0700

    form validation with formValues

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,7 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 //reduxForm like connect, Field is comp
 class StreamCreate extends Component {
 
-  renderInput(formProps){
+  renderInput(formProps){ //cl
     return (
       <div className="field">
         <label>{formProps.label}</label>
@@ -12,14 +12,26 @@ class StreamCreate extends Component {
     )
   }
 
+  onSubmit(formValues){ //cl
+
+  }
+
   render() {
     return (
-      <form className="ui form">
+      <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <Field name="title" component={this.renderInput} label="Enter title"/>
         <Field name="description" component={this.renderInput} label="Enter description"/>
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
+}
+
+const validate = (formValues) => {
+  const error = {};
+  if(!formValues.title) error.title = 'enter a title please';
+  if(!formValues.description) error.description = 'enter description please';
+  return error;  
 }
 
 export default reduxForm({ //it give tone of props after init in here

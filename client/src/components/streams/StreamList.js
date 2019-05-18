7b67bc6a9e12dc776cae7eb91d0657da07f92bd1
mazdak <nazemi.works@gmail.com>
2019-05-18 10:14:07 -0700

    initial loading component in dom

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return (
-    <div>
-      list
-    </div>
-  )
+class StreamList extends Component {
+  
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
-export default StreamList;
+export default connect(null,{fetchStreams})(StreamList);

44283ce5869b170b8f4037fd727cebcd07b33e44
mazdak <nazemi.works@gmail.com>
2019-05-18 02:49:10 -0700

    form crate use Field and reduxForm formProps is main player

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+//reduxForm like connect, Field is comp
+class StreamCreate extends Component {
 
-const StreamCreate = () => {
-  return (
-    <div>
-      create
-    </div>
-  )
+  renderInput(formProps){
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input}/>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title"/>
+        <Field name="description" component={this.renderInput} label="Enter description"/>
+      </form>
+    );
+  }
 }
 
-export default StreamCreate;
+export default reduxForm({ //it give tone of props after init in here
+  form: 'streamCreate'
+})(StreamCreate);

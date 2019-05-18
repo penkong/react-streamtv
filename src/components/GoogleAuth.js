258d68ab2e7ea750efbf915b9169fd32f28bc3de
mazdak <nazemi.works@gmail.com>
2019-05-17 23:01:30 -0700

    auth before redux onready change

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -3,19 +3,40 @@ import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
 
+  state = { isSignedIn : null };
+
   componentDidMount() {
+    //gapi.auth2.getAuthInstance this is obj we want
     window.gapi.load('client:auth2' , ()=>{
       window.gapi.client.init({
         clientId: '891354093350-97k9q1batjta6ei0pcv25plh4l1r7um9.apps.googleusercontent.com',
         scope: 'email' 
-      })
-    })
+      }).then(()=>{// let user sign in out status ...
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.onAuthChange(); //catch for first initialize
+        this.auth.isSignedIn.listen(this.onAuthChange);
+      });
+    });
   }
   
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  }
+
+  renderAuthButton(){
+    if(this.state.isSignedIn === null) {
+      return <div>i dont knewo</div>
+    } else if (this.state.isSignedIn) {
+      return <div>ddddd</div>
+    } else {
+      return <div> dddddffffff</div>
+    }
+  }
+
   render() {
     return (
       <div>
-        GoogleAuth
+        {this.renderAuthButton()}
       </div>
     );
   }

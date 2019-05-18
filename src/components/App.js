017b0111971ad77051273d2d31364942870a231d
mazdak <nazemi.works@gmail.com>
2019-05-17 21:41:51 -0700

    chaining route skeleton for start

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+
 import StreamList from './streams/StreamList';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -9,15 +12,18 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={StreamList}/>
-        <Route path="/streams/new" exact component={StreamCreate}/>
-        <Route path="/streams/edit" exact component={StreamEdit}/>
-        <Route path="/streams/delete" exact component={StreamDelete}/>
-        <Route path="/streams/show" exact component={StreamShow}/>
-      </div>
-    </BrowserRouter>
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList}/>
+          <Route path="/streams/new" exact component={StreamCreate}/>
+          <Route path="/streams/edit" exact component={StreamEdit}/>
+          <Route path="/streams/delete" exact component={StreamDelete}/>
+          <Route path="/streams/show" exact component={StreamShow}/>
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 

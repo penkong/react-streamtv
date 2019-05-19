645f3718eaa532cf2a46e8aa8a70784902ce1170
mazdak <nazemi.works@gmail.com>
2019-05-19 03:09:26 -0700

    show stream

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 //because of we make history we use plain router
-import { Router, Route } from 'react-router-dom';
+import { Router, Route , Switch } from 'react-router-dom';
 
 import history from '../history'; //for prog nav
 import Header from './Header';
@@ -11,18 +11,20 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 
-
+//switch help show one route at time
 const App = () => {
   return (
     <div className="ui container">
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList}/>
-          <Route path="/streams/new" exact component={StreamCreate}/>
-          <Route path="/streams/edit/:id" exact component={StreamEdit}/>
-          <Route path="/streams/delete/:id" exact component={StreamDelete}/>
-          <Route path="/streams/show" exact component={StreamShow}/>
+          <Switch> 
+            <Route path="/" exact component={StreamList}/>
+            <Route path="/streams/new" exact component={StreamCreate}/>
+            <Route path="/streams/edit/:id" exact component={StreamEdit}/>
+            <Route path="/streams/delete/:id" exact component={StreamDelete}/>
+            <Route path="/streams/:id" exact component={StreamShow}/>
+          </Switch>
         </div>
       </Router>
     </div>

c0228a4928ce6ed997799c2c673d25902edbc533
mazdak <nazemi.works@gmail.com>
2019-05-19 01:56:16 -0700

    modal ui complete next functionality

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import history from '../../history';
+import Modal from '../Modal';
+
 
 const StreamDelete = () => {
+
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">cancel</button>
+    </React.Fragment>
+  );
+
   return (
     <div>
-      delete
+      <Modal 
+        title="Delete Stream"
+        content = "Are you sure you want delete stream?"
+        actions = {actions}
+        onDismiss = {()=>history.push('/')}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default StreamDelete;
